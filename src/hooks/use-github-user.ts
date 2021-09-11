19c183003b71109577a79b5dcc7609dfbcdf94de
leonardo bravo <j.leonardo.bravo@gmail.com>
2021-09-11 14:48:26 -0500

    feat: add useGithubUser and githubUser types

---
+++ src/hooks/use-github-user.ts
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import { useQuery, gql } from "@apollo/client";
+import { User } from "types/github-user";
+import { searchRepo } from "handlers/search-bar";
+
+const ALL_REPOSITORIES = gql`
+  query AllRepositories($username: String!) {
+    user(login: $username) {
+      login
+      name
+      createdAt
+      avatarUrl
+      bio
+      repositories(first: 60) {
+        nodes {
+          name
+          id
+          description
+          nameWithOwner
+          url
+        }
+      }
+    }
+  }
+`;
+
+const initialState: User = {
+  __typename: "",
+  avatarUrl: "",
+  bio: "",
+  createAt: "",
+  login: "",
+  name: "",
+  repositories: { __typename: "", nodes: [] },
+};
+
+export default function useGithubUser(username: string) {
+  const [user, setUser] = useState(initialState);
+  const [repoSearch, setRepoSearch] = useState({
+    query: "",
+    repos: initialState.repositories.nodes,
+  });
+  const { data } = useQuery(ALL_REPOSITORIES, {
+    variables: { username: username },
+  });
+
+  useEffect(() => {
+    if (data) setUser(data.user);
+  }, [data]);
+
+  return {
+    user,
+    repoSearch,
+    searchRepo: (e: string) => {
+      searchRepo(e, user.repositories.nodes, setRepoSearch);
+    },
+  };
+}
---
+++ src/types/github-user.ts
@@ -0,0 +1,30 @@
+import { Dispatch, SetStateAction } from "react";
+
+export type Repo = {
+  __typename: string;
+  description: string | undefined;
+  id: string;
+  name: string;
+  nameWithOwner: string;
+  url: string;
+};
+
+export type User = {
+  __typename: string;
+  avatarUrl: string;
+  bio: string;
+  createAt: string;
+  login: string;
+  name: string;
+  repositories: {
+    __typename: string;
+    nodes: Repo[];
+  };
+};
+
+export type RepoSearch = {
+  query: string;
+  repos: Repo[];
+};
+
+export type SetRepoSearch = Dispatch<SetStateAction<RepoSearch>>;
